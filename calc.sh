e82470bd97463b9dd10c10972334e30b8dac96f7
Amandeep Singh <amandeep100393@gmail.com>
2020-03-23 21:00:22 -0400

    Checking if correct operation & error msg for unsupported selection

--- calc.sh
+++ calc.sh
@@ -12,9 +12,25 @@ echo "Subtract"
 echo "Exit"
 # Then, capture the user selection.
 read user_selection
-#temp to check is selction is correct. remove after check
-#echo $user_selection
-# If the selection matches a supported operation, execute the operation.
+  #temp to check is selction is correct. remove after check
+  #echo $user_selection
 
-# If the selection does not match a support operation, display an error message.
+# If the selection matches a supported operation, execute the operation.
+case $user_selection in 
+  "Add")
+    #echo "User selected Add"
+    ;;
+    "Subtract")
+    #echo "User selected Subtract"
+    ;;
+    "Exit")
+    #echo "User selected Exit"
+    ;;
+
+    # If the selection does not match a support operation, display an error message.
+    *)
+    echo "Sorry, unsupported operation: $user_selection"
+    echo "Please try again"
+esac
+
 # When the operation is complete, redisplay the menu.
