796f0a0cecb3f88601f79ec61e03de448bc390db
Amandeep Singh <amandeep100393@gmail.com>
2020-03-23 21:31:02 -0400

    Added whilee loop to prompt menu to user untill exit

--- calc.sh
+++ calc.sh
@@ -2,47 +2,52 @@
 
 # When the program is first loaded, display a greeting to the user.
 echo "Welcome to the Calculator Program"
-# Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
-echo "Please select the operation you want to execute"
-echo "Add"
-echo "Subtract"
-echo "Exit"
-# Then, capture the user selection.
-read user_selection
-  #temp to check is selction is correct. remove after check
-  #echo $user_selection
 
-# If the selection matches a supported operation, execute the operation.
-case $user_selection in 
-  "Add")
-    #echo "User selected Add"
-    echo "Please enter the First number"
-    read first_number
-    echo "Please enter the Second number"
-    read second_number
-    echo "The sum of $first_number and $second_number is :"
-    expr $first_number + $second_number
-    ;;
-    "Subtract")
-    #echo "User selected Subtract"
-    echo "Please enter the First number"
-    read first_number
-    echo "Please enter the Second number"
-    read second_number
-    echo "The difference of $first_number and $second_number is :"
-    expr $first_number - $second_number
-    ;;
-    "Exit")
-    #echo "User selected Exit"
-    ;;
+#To prompt user everytime an operation is done, we introduced an infitinte while loop
+while true; do
+  # Then, display a menu that outlines the possible operations:
+    # Add
+    # Subtract
+    # Exit
+  echo "Please select the operation you want to execute"
+  echo "Add"
+  echo "Subtract"
+  echo "Exit"
+  # Then, capture the user selection.
+  read user_selection
+    #temp to check is selction is correct. remove after check
+    #echo $user_selection
 
-    # If the selection does not match a support operation, display an error message.
-    *)
-    echo "Sorry, unsupported operation: $user_selection"
-    echo "Please try again"
-esac
+  # If the selection matches a supported operation, execute the operation.
+  case $user_selection in 
+    "Add")
+      #echo "User selected Add"
+      echo "Please enter the First number"
+      read first_number
+      echo "Please enter the Second number"
+      read second_number
+      echo "The sum of $first_number and $second_number is :"
+      expr $first_number + $second_number
+      ;;
+      "Subtract")
+      #echo "User selected Subtract"
+      echo "Please enter the First number"
+      read first_number
+      echo "Please enter the Second number"
+      read second_number
+      echo "The difference of $first_number and $second_number is :"
+      expr $first_number - $second_number
+      ;;
+      "Exit")
+      #echo "User selected Exit"
+      ;;
 
-# When the operation is complete, redisplay the menu.
+      # If the selection does not match a support operation, display an error message.
+      *)
+      echo "Sorry, unsupported operation: $user_selection"
+      echo "Please try again"
+      ;;
+  esac
+
+  # When the operation is complete, redisplay the menu.
+done
