df7522138d3090939d9311d63056c23da4f42e70
Amandeep Singh <amandeep100393@gmail.com>
2020-03-23 20:38:49 -0400

    Added greeting message

--- calc.sh
+++ calc.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env bash
 
 # When the program is first loaded, display a greeting to the user.
+echo "Welcome to the Calculator Program"
 # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
   # Exit
 # Then, capture the user selection.
-# If the selection matches a supported operation, execute the operation.
+# If the selection matches a suppor ted operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
