2a7623a81952bb0a4a0c0316f9885d80dc75512f
Amandeep Singh <amandeep100393@gmail.com>
2020-03-23 20:42:20 -0400

    Added menu with add,subtract & exit options

--- calc.sh
+++ calc.sh
@@ -6,6 +6,10 @@ echo "Welcome to the Calculator Program"
   # Add
   # Subtract
   # Exit
+echo "Please select the operation you want to execute"
+echo "Add"
+echo "Subtract"
+echo "Exit"
 # Then, capture the user selection.
 # If the selection matches a suppor ted operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
